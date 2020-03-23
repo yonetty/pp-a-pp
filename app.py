b26da05d8203d9db48dbbcacb43463989526b6a4
yonetty <tyonekubo@outlook.jp>
2020-03-23 22:35:49 +0900

    add index.html

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Top Page"
+    html = render_template("index.html")
+    return html
 
 
 if __name__ == "__main__":
